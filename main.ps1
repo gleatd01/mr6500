763cd82e12620bfff5170d4f442e7ef539394775
gleatd01 <taylor@buzzedtop.com>
2024-12-30 10:48:03 -0500

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -4,3 +4,6 @@ Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 scoop bucket add extras
 scoop install miniconda3
 
+Get-Process -Id $PID | Select-Object -ExpandProperty Path | ForEach-Object { Invoke-Command { & "$_" } -NoNewScope }
+
+conda create -n netgear python=3.12
